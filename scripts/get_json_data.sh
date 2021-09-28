5e3e9c45ccca5b5c5839c5198f3a3cf29e067611
Hirokazu Chiba <hchiba1@gmail.com>
2021-09-28 22:51:47 +0900

    Add scripts/

---
+++ scripts/get_json_data.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -L https://script.google.com/macros/s/AKfycbxHqo8jUDQAmmgQxIba6jK_Je9tY3tdbTALvo_jfzgijm9sPkKYJ91D6y2W4SvIbGfr/exec > json/engines.json
+curl -L https://script.google.com/macros/s/AKfycbxj25M63b1j63g_quAJwbLlqVTYpVt5o7dbD-9R7qHLbL-iLzzmxX8kB5elBzlbS1VB/exec > json/references.json
