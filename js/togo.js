e9856313971bf625a36f2310ec46617bc836903e
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2021-10-06 15:16:34 +0900

    Reformat HTML and refactor JS

--- js/togo.js
+++ js/togo.js
@@ -1,22 +1,15 @@
 // namespace
-var togo = {};
+let togo = {};
 
 // id
 togo.id = 1;
-
-// issue ID
-togo.issueId = function () {
-  var id = togo.id;
-  togo.id++;
-  return id;
-}
 
 // create tabs
 togo.createTabs = (tabs) => {
   tabs.forEach((element) => {
     title = element.title;
     page = element.page;
-    const id = togo.issueId();
+    const id = togo.id++;
     element.id = id;
     togo.addTabButton(title, id);
     togo.addTabContent(page, id);
@@ -31,20 +24,18 @@ togo.createTabs = (tabs) => {
 
 // add tab button
 togo.addTabButton = function (title, id) {
-  var buttonId = 'tab_button-' + id;
-  var tag = '<button id="' + buttonId + '" class="tab_button">' + title + '</button>';
+  let buttonId = 'tab_button-' + id;
+  let tag = `<button id="${buttonId}" class="tab_button">${title}</button>`;
   $('#tabs').append(tag);
   $('#' + buttonId).click(
-    function () {
-      togo.openTab(id);
-    }
+    () => togo.openTab(id)
   );
 }
 
 // add tab content
 togo.addTabContent = function (page, id) {
-  var panelId = 'tab_content-' + id;
-  var tag = '<div id="' + panelId + '" class="tab_content"></div>'
+  let panelId = 'tab_content-' + id;
+  let tag = `<div id="${panelId}" class="tab_content"></div>`;
   $('#contents').append(tag);
   $('#' + panelId).load(page + '.html');
 }
@@ -55,40 +46,9 @@ togo.openTab = function (id) {
   $('.tab_button').css('border-bottom', 'none');
   $('.tab_button:hover').css('border-bottom', '2px solid #005cab');
   $('.tab_content').css('display', 'none');
-  $('#tab_button-' + id).css('color', '#005cab');
-  $('#tab_button-' + id).css('border-bottom', '2px solid #005cab');
+  $('#tab_button-' + id).css('color', '#005cab').css('border-bottom', '2px solid #005cab');
   $('#tab_content-' + id).css('display', 'block');
 }
-
-// headers
-togo.getHeaders = function (result) {
-  var headers = [];
-  result.head.vars.forEach(
-    function (element) {
-      headers.push(element);
-    }
-  );
-  return headers;
-}
-
-// results
-togo.getResult = function (result, headers) {
-  var array = [];
-  result.results.bindings.forEach(
-    function (element) {
-      var row = {};
-      headers.forEach(
-        function (header) {
-          var value = element[header].value;
-          row[header] = value;
-        }
-      );
-      array.push(row);
-    }
-  );
-  return array;
-}
-
 
 // create db table
 togo.createDbTable = function (id) {
@@ -103,12 +63,14 @@ togo.createDbTable = function (id) {
     }
   ).then(
     function (result) {
-      let content = '<thead><tr><th>No.</th><th>Name</th><th>Company/ Organization</th><th>First Release</th><th>Model</th><th>Implementation</th><th>Query Language</th><th>Source code</th><th>History</th></tr></thead>'
+      let content = '<thead><tr><th>No.</th><th>Name</th><th>Company/ Organization</th><th>First Release</th>' +
+        '<th>Model</th><th>Implementation</th><th>Query Language</th><th>Source code</th><th>History</th></tr></thead>';
+      
       result.forEach(
         (row, i) => {
-          if(row.Hide != '1') {
+          if(row.Hide !== '1') {
             row.number = i + 1;
-            var lineTag = togo.createDbLineTag(row);
+            let lineTag = togo.createDbLineTag(row);
             content += lineTag;
           }
         }
@@ -126,50 +88,50 @@ togo.createDbTable = function (id) {
 
 // create DB line tag
 togo.createDbLineTag = function (object) {
-  var keys = [
+  let keys = [
     'number', 'Name', 'Company', 'First release', 'Data model', 'Implementation', 'Query language', 'Source code', 'Comment'
   ]
-  var tag = togo.createLineTag(object, keys);
+  let tag = togo.createLineTag(object, keys);
   return tag;
 }
 
 // create line tag
 togo.createLineTag = function (object, keys) {
-  var tag = '';
+  let tag = '';
   keys.forEach(
-    function (key) {
+    (key) => {
       if (key in object) {
-        var value = object[key];
+        let value = object[key];
         if (key === 'Name') {
-          var url = object.URL;
+          let url = object.URL;
           if (url != null) {
-            value = '<a href="' + url + '" target="_blank">' + value + '</a>';
+            value = `<a href="${url}" target="_blank">${value}</a>`;
             // value = '<img src="img/jena.png" height="50" />';
           }
         }
         if (key === 'Source code') {
-          value = '<a href="' + value + '" target="_blank">' + 'GitHub' + '</a>';
+          value = `<a href="${value}" target="_blank">GitHub</a>`;
         }
-        tag += '<td>' + value + '</td>'
+        tag += `<td>${value}</td>`;
       } else {
         tag += '<td></td>';
       }
     }
   );
-  tag = '<tr>' + tag + '</tr>';
+  tag = `<tr>${tag}</tr>`;
   return tag;
 }
 
 // create db table
 togo.createLinkTable = function (id) {
-  var tag = '<tr><th>Name</th><th>URL</th></tr>';
+  let tag = '<tr><th>Name</th><th>URL</th></tr>';
   $('#' + id).html(tag);
   $.getJSON(
     'json/links.json',
-    function (data) {
+    (data) => {
       data.forEach(
-        function (element) {
-          var tag = togo.createLineTag(element, ['name', 'url']);
+        (element) => {
+          let tag = togo.createLineTag(element, ['name', 'url']);
           $('#' + id).append(tag);
         }
       );
@@ -190,7 +152,7 @@ togo.createPaperTable = function (id) {
     }
   ).then(
     function (result) {
-      var content = '<thead><tr><th>Tag</th><th>Year</th><th>Paper</th></tr></thead>';
+      let content = '<thead><tr><th>Tag</th><th>Year</th><th>Paper</th></tr></thead>';
       result.forEach(
         (row) => {
           content += togo.createPaperLineTag(row.Tag, row.Paper, row.Year, row.URL);
@@ -203,27 +165,25 @@ togo.createPaperTable = function (id) {
 
 // create paper line tag
 togo.createPaperLineTag = function (tag, paper, year, url) {
-  var line = '<tr><td>' + tag + '</td><td>' + year + '</td>';
-  var paperTag = paper;
+  let paperTag = paper;
   if (url !== '') {
-    paperTag = '<a href="' + url + '" target="_blank">' + paperTag + '</a>';
+    paperTag = `<a href="${url}" target="_blank">${paperTag}</a>`;
   }
-  paperTag = '<td>' + paperTag + '</td>';
-  line = line + paperTag + '</tr>';
-  return line;
+  paperTag = `<td>${paperTag}</td>`;
+  return `<tr><td>${tag}</td><td>${year}</td>${paperTag}</tr>`;
 }
 
 // create reference table
 togo.createReferenceTable = function (id) {
-  var tag = '<tr><th>Authors</th><th>Year</th><th>Title</th><th>Journal</th></tr>';
+  let tag = '<tr><th>Authors</th><th>Year</th><th>Title</th><th>Journal</th></tr>';
   $('#' + id).html(tag);
   $.getJSON(
     'json/references.json',
-    function (data) {
+    (data) => {
       data.forEach(
         function (element) {
-          var keys = ['authors', 'year', 'title', 'journal'];
-          var tag = togo.createLineTag(element, keys);
+          let keys = ['authors', 'year', 'title', 'journal'];
+          let tag = togo.createLineTag(element, keys);
           $('#' + id).append(tag);
         }
       );
