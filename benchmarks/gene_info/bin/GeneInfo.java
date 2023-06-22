a1d013c8f99e97d1334a0bc94d8f9f5b82358382
Hirokazu Chiba <hchiba1@gmail.com>
2023-06-22 09:10:59 +0900

    Rename to bin/GeneInfo.java

--- benchmarks/gene_info/bin/GeneInfo.java
+++ benchmarks/gene_info/bin/GeneInfo.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class GeneDataProcessor {
+public class GeneInfo {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.exit(1);
